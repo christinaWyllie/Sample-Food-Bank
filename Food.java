231ed7c24e65a9ef279967372fa78467e73a4ea4
sanikashendye <101202484+sanikashendye@users.noreply.github.com>
2022-03-30 15:36:04 -0600

    Update Food.java

--- Food.java
+++ Food.java
@@ -1,1 +1,23 @@
 //food
+import java.sql.*;
+
+public class Food{
+  private Connection dbConnect;
+  private ResultSet results;
+  private int[][] calorieTable = new int[4][
+  
+  public Food(){
+    initializeConnection();
+    
+  }
+  
+  public void initializeConnection(){
+    try{
+      this.dBConnect = DriverManager.getConnection(databaseURL, "student", "ensf409");
+    }
+    catch(SQLException e){
+      e.printStackTrace(); //may need to be changed 
+    }
+  }
+  
+  public void getCalorieInfo()
