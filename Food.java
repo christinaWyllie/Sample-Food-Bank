e837977ce510b886c45141d15de246c8a1662540
sanikashendye <101202484+sanikashendye@users.noreply.github.com>
2022-03-30 17:08:16 -0600

    Update Food.java

--- Food.java
+++ Food.java
@@ -2,14 +2,16 @@
 
 public class Food{
   private final String name;
+  private final itemID;
   private final Nutrition nutritionValue;
   
-  public Food(String n, int g, int f, int p, int o, int c){
+  public Food(int itemID, String n, int g, int f, int p, int o, int c){
     this.name = n;
     int grain = (int)((double)(g/100) * c));
     int fv = (int)((double)(f/100) * c));
     int protein = (int)((double)(p/100) * c));
     int other = (int)((double)(o/100) * c));
     this.nutritionValue = new Nutrition(grain, protein, fv, other, c);
+    this.itemID = itemID;
   }
 }
