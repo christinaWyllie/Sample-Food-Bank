89ab3c749b835872368e975415cc9475a6396b3f
christinaWyllie <100972547+christinaWyllie@users.noreply.github.com>
2022-03-30 14:51:00 -0600

    Update HamperNutrition.java

--- HamperNutrition.java
+++ HamperNutrition.java
@@ -1,5 +1,22 @@
 package edu.ucalgary.ensf409;
 
 public class HamperNutrition{
-
+  private LinkedList[] hamper = new LinkedList();
+  
+  public HamperNutrition(){
+    
+  } 
+  
+  public void calculate(){
+    
+  }
+  
+  public void addToHamper(){
+    
+  }
+  
+  public void checkInventory() throws NotEnoughInventoryException{
+    
+  } 
+  
 }
