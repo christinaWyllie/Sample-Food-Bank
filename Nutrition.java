50a927fff0a5997a1bf174679e192f138e83d952
sanikashendye <101202484+sanikashendye@users.noreply.github.com>
2022-03-30 14:37:23 -0600

    Create Nutrition.java

---
+++ Nutrition.java
@@ -0,0 +1,41 @@
+//Nutrition
+
+
+public class Nutrition{
+  private final int GRAIN;
+  private final int PROTEIN;
+  private final int FV;
+  private final int OTHER;
+  private final int CALORIES;
+  
+  public Nutrition(int g, int p, int f, int o, int c)
+  {
+    this.GRAIN = g;
+    this.PROTEIN = p;
+    this.FV = f;
+    this.OTHER = o;
+    this.CALORIES = c;
+  }
+  
+  public int getGrain(){
+    return this.GRAIN;
+  }
+  
+  public int getProtein(){
+    return this.PROTEIN;
+  }
+    
+  public int getFV(){
+    return this.FV;
+  }
+  
+  public int getOther(){
+    return this.OTHER;
+  }
+  
+  public int getCalories(){
+    return this.CALORIES;
+  }
+}
+  
+  
