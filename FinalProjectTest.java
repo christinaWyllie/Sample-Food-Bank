b500b37fbf537e52c35843d5e29f3e9dae4a3883
christinaWyllie <100972547+christinaWyllie@users.noreply.github.com>
2022-03-31 12:04:39 -0600

    Update FinalProjectTest.java

--- FinalProjectTest.java
+++ FinalProjectTest.java
@@ -262,6 +262,8 @@ public class FinalProjectTest {
    public void testRemoveInventory() {
         //testing the removal of items from food, parallel to the database
 	   
+	   //create food objects
+	   
 	   String name1= "Wheat bread, loaf";
 	   int g1 = 96;
 	   int f1 = 0;
@@ -276,7 +278,7 @@ public class FinalProjectTest {
 	   int p2 = 4;
 	   int o2 = 0;
 	   int c2 = 2192;
-	   Food food1 = Food(name2, g2, f2, p2 o2, c2);
+	   Food food1 = new Food(name2, g2, f2, p2 o2, c2);
 	   
 	   String name3= "Wheat bread, loaf";
 	   int g3 = 96;
@@ -284,21 +286,41 @@ public class FinalProjectTest {
 	   int p3 = 4;
 	   int o3 = 0;
 	   int c3 = 2192;
-	   Food food1 = Food(name3, g3, f3, p3, o3, c3);
+	   Food food2 = new Food(name3, g3, f3, p3, o3, c3);
 
 	   String name4= "Orange, dozen";
-	   int g1 = 0;
-	   int f1 = 0;
-	   int p1 = 4;
-	   int o1 = 0;
-	   int c1 = 2192;
-	   Food food1 = Food(name1, g1, f1, p1, o1, c1);
+	   int g4 = 0;
+	   int f4 = 100;
+	   int p4 = 0;
+	   int o4 = 0;
+	   int c4 = 864;
+	   Food food4 = new Food(name4, g4, f4, p4, o4, c4);
 	   
-	('Orange, dozen', 0, 100, 0, 0, 864), 
-	('Orange, dozen', 0, 100, 0, 0, 864), 
-	('Eggs, dozen', 0, 0, 9, 91, 864), 
-		
-		
+	   String name5= "Orange, dozen";
+	   int g5 = 0;
+	   int f5 = 100;
+	   int p5 = 0;
+	   int o5 = 0;
+	   int c5 = 864;
+	   Food food5 = new Food(name5, g5, f5, p5, o5, c5);
+	   
+	   String name6 = "Eggs, dozen";
+	   int g6 = 0;
+	   int f6 = 0;
+	   int p6 = 9;
+	   int o6 = 91;
+	   int c6 = 864;
+	   Food food6 = new Food(name6, g6, f6, p6, o6, c6);
+	   
+	   LinkedList<Food> foodList = new LinkedList<Food>(); 
+	   foodList.add(food1);
+	   foodList.add(food2);
+	   foodList.add(food3);
+	   foodList.add(food4);
+	   foodList.add(food5);
+	   foodList.add(food6);
+	   
+	   
 
         assertEquals("Value of contents did not match what was expected: ", givenData, actualContents);
     }
