f2f575239d7ed8150f7cd58a33bd62ba5eadc3dc
sanikashendye <101202484+sanikashendye@users.noreply.github.com>
2022-03-31 09:42:42 -0600

    Update Food.java

--- Food.java
+++ Food.java
@@ -4,7 +4,7 @@ public class Food{
   private final String name;
   private final itemID;
   private final Nutrition nutritionValue;
-  
+  //private static LinkedList<Food> inventory = new LinkedList<Food>();
   public Food(int itemID, String n, int g, int f, int p, int o, int c){
     this.name = n;
     int grain = (int)((double)(g/100) * c));
