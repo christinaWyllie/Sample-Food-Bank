28f128a225da35eb30201277443315f46c7d89f3
sanikashendye <101202484+sanikashendye@users.noreply.github.com>
2022-03-31 15:05:49 -0600

    Update FinalProjectTest.java

--- FinalProjectTest.java
+++ FinalProjectTest.java
@@ -238,52 +238,58 @@ public class FinalProjectTest {
 	   //create food objects
 	   
 	   String name1= "Wheat bread, loaf";
+	   int id1 = 1;
 	   int g1 = 96;
 	   int f1 = 0;
 	   int p1 = 4;
 	   int o1 = 0;
 	   int c1 = 2192;
-	   Food food1 = Food(name1, g1, f1, p1, o1, c1);
+	   Food food1 = Food(id1, name1, g1, f1, p1, o1, c1);
 	   
 	   String name2 = "Wheat bread, loaf";
+	   int id2 = 2;
 	   int g2 = 96;
 	   int f2 = 0;
 	   int p2 = 4;
 	   int o2 = 0;
 	   int c2 = 2192;
-	   Food food1 = new Food(name2, g2, f2, p2 o2, c2);
+	   Food food1 = new Food(id2, name2, g2, f2, p2 o2, c2);
 	   
 	   String name3= "Wheat bread, loaf";
+	   int id3 = 3;
 	   int g3 = 96;
 	   int f3 = 0;
 	   int p3 = 4;
 	   int o3 = 0;
 	   int c3 = 2192;
-	   Food food2 = new Food(name3, g3, f3, p3, o3, c3);
+	   Food food2 = new Food(id3, name3, g3, f3, p3, o3, c3);
 
 	   String name4= "Orange, dozen";
+	   int id4 = 4;
 	   int g4 = 0;
 	   int f4 = 100;
 	   int p4 = 0;
 	   int o4 = 0;
 	   int c4 = 864;
-	   Food food4 = new Food(name4, g4, f4, p4, o4, c4);
+	   Food food4 = new Food(id4, name4, g4, f4, p4, o4, c4);
 	   
 	   String name5= "Orange, dozen";
+	   int id5 = 5;
 	   int g5 = 0;
 	   int f5 = 100;
 	   int p5 = 0;
 	   int o5 = 0;
 	   int c5 = 864;
-	   Food food5 = new Food(name5, g5, f5, p5, o5, c5);
+	   Food food5 = new Food(id5, name5, g5, f5, p5, o5, c5);
 	   
 	   String name6 = "Eggs, dozen";
+	   int id6 = 6;
 	   int g6 = 0;
 	   int f6 = 0;
 	   int p6 = 9;
 	   int o6 = 91;
 	   int c6 = 864;
-	   Food food6 = new Food(name6, g6, f6, p6, o6, c6);
+	   Food food6 = new Food(id6, name6, g6, f6, p6, o6, c6);
 	   
 	   LinkedList<Food> foodList = new LinkedList<Food>(); 
 	   foodList.add(food1);
@@ -305,53 +311,60 @@ public class FinalProjectTest {
 	public void testOrderFormConstructor() {
 		
 	//create food objects 
+	   	   
 	   String name1= "Wheat bread, loaf";
+	   int id1 = 1;
 	   int g1 = 96;
 	   int f1 = 0;
 	   int p1 = 4;
 	   int o1 = 0;
 	   int c1 = 2192;
-	   Food food1 = Food(name1, g1, f1, p1, o1, c1);
+	   Food food1 = Food(id1, name1, g1, f1, p1, o1, c1);
 	   
 	   String name2 = "Wheat bread, loaf";
+	   int id2 = 2;
 	   int g2 = 96;
 	   int f2 = 0;
 	   int p2 = 4;
 	   int o2 = 0;
 	   int c2 = 2192;
-	   Food food1 = new Food(name2, g2, f2, p2 o2, c2);
+	   Food food1 = new Food(id2, name2, g2, f2, p2 o2, c2);
 	   
 	   String name3= "Wheat bread, loaf";
+	   int id3 = 3;
 	   int g3 = 96;
 	   int f3 = 0;
 	   int p3 = 4;
 	   int o3 = 0;
 	   int c3 = 2192;
-	   Food food2 = new Food(name3, g3, f3, p3, o3, c3);
+	   Food food2 = new Food(id3, name3, g3, f3, p3, o3, c3);
 
 	   String name4= "Orange, dozen";
+	   int id4 = 4;
 	   int g4 = 0;
 	   int f4 = 100;
 	   int p4 = 0;
 	   int o4 = 0;
 	   int c4 = 864;
-	   Food food4 = new Food(name4, g4, f4, p4, o4, c4);
+	   Food food4 = new Food(id4, name4, g4, f4, p4, o4, c4);
 	   
 	   String name5= "Orange, dozen";
+	   int id5 = 5;
 	   int g5 = 0;
 	   int f5 = 100;
 	   int p5 = 0;
 	   int o5 = 0;
 	   int c5 = 864;
-	   Food food5 = new Food(name5, g5, f5, p5, o5, c5);
+	   Food food5 = new Food(id5, name5, g5, f5, p5, o5, c5);
 	   
 	   String name6 = "Eggs, dozen";
+	   int id6 = 6;
 	   int g6 = 0;
 	   int f6 = 0;
 	   int p6 = 9;
 	   int o6 = 91;
 	   int c6 = 864;
-	   Food food6 = new Food(name6, g6, f6, p6, o6, c6);
+	   Food food6 = new Food(id6, name6, g6, f6, p6, o6, c6);
 	   
 	   LinkedList<Food> foodList = new LinkedList<Food>(); 
 	   foodList.add(food1);
