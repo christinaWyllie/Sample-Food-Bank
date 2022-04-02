ebe9b13a866a3e4ff50b880f5dd787df1bd1efa3
MaitryRohit <101202431+MaitryRohit@users.noreply.github.com>
2022-04-02 16:06:18 -0600

    Update Application.java
    jziodhnbzdk;jnhr;ozbgndzfjbnO:RBGrs;jozbfkj;zbrs;gh;osidbndf

--- Application.java
+++ Application.java
@@ -6,22 +6,28 @@ public class Application{
   private int numHamper = 1;
   private Iventory inventory;
   private ArrayList<Family> families;
-  private Data
+  private ArrayList<HamperNutrition> hamper;
   
-  ArrayList<HamperNutrition> hamper;
-  
-  public Application(){
-    this.numHamper = ;
-    
-    hamper = new ArrayList(numHamper);
-  
+  public Application(int[] var){
+    this.inventory = new Inventory();
+    int[][] calorieTable = inventory.getCalorieTable();
+    this.families = new ArrayList<Family>();
+    this.families.add(new Family(var, calorieTable));
+    this.hamper = new ArrayList<HamperNutrition>();
+    this.hamper.add(new HamperNutrition(this.families.get(numHampers-1).getTotal())); //What nutriton object do I send in?
+  }
+ 
+  public void addHamper(int[] var) {
+     this.numHampers++;
+     this.families.add(new Family(var, this.inventory.calorieTable()));
+     this.hamper.add(new HamperNutrition(this.families.get(numHampers-1).getTotal())); //Still dont know what nutriton object im sending 
   }
   
-  public void addHamper(){
-    
-  }
-  
-  
-  public 
-  
+  public int[] getUserInput(int index) {return this.families.get(index).getFamilyMembers();};
+  public int getNumHamper() {return this.numHamper;};
+  public ArrayList<Family> getFamiles() {return this.familes;};
+  public Arraylist<HamperNutrition> getHampers() {return this.hamper;};
+  public Family getFamily(int index) {return this.familes.get(index);};
+  public LinkedList<Food> getHamper(int index) {return this.hamper.get(index).getHamper();};
+  public Inventory getInventory() {return this.inventory;};
 }
