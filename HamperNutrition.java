622a142a685ababb69c49fd7fce884f9bffa7b53
christinaWyllie <100972547+christinaWyllie@users.noreply.github.com>
2022-04-04 17:23:43 -0600

    Update HamperNutrition.java

--- HamperNutrition.java
+++ HamperNutrition.java
@@ -1,17 +1,18 @@
 package edu.ucalgary.ensf409;
 
 public class HamperNutrition{
-  private LinkedList[] hamper = new LinkedList();
+  private LinkedList[] hamper = new LinkedList<Food>();
   private Inventory inventory;
   
-  public HamperNutrition(){
+  public HamperNutrition(Nutrition nutrition){
+    
   } 
   
   public void calculate(){
     
   }
   
-  public void addToHamper(){
+  public void addToHamper(Food food){
     
   }
   
@@ -19,7 +20,20 @@ public class HamperNutrition{
     
   } 
   
+  public void createBestHamper(){
   
+  }
   
+  public void removeFromInventory(){
+    
+  }
+  
+  public Inventory getInventory(){
+    return this.inventory;
+  }
+  
+  public void setInventory(Inventory i){
+    this.inventory = i;
+  }
   
 }
