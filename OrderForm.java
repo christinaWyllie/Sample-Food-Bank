629c9382580f125c1427dedf8d2d4831490cd278
christinaWyllie <100972547+christinaWyllie@users.noreply.github.com>
2022-04-03 20:16:44 -0600

    Update OrderForm.java

--- OrderForm.java
+++ OrderForm.java
@@ -2,17 +2,27 @@
 package edu.ucalgary.ensf409;
 
 public class OrderForm implements FormatOutput{
-  private Application app;
+  private Family family;
+  private Inventory inventory;
+  private HamperNutrition hamper;
   
-  public OrderFrom()throws IOException{
-    
+  public OrderFrom(Family f, Inventory i, HamperNutrition h)throws IOException{
+    this.family = f;
+    this.inventory = i;
+    this.hamper = h;
   }
   
-  public void removeInventory(){
+  public void removeFromDataBase(){
     
   } 
   
-  public void print(){
+  public void print()throws IOException{
+    String fileName = "orderForm.txt";
+    FileWriter write = null;
+    try{
+      write = new FileWriter(new File(fileName));
+      
+    }
     
   } 
   
