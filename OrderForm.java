a25c9c91350aa5a6626a2d66ffdd7a3dbb569972
christinaWyllie <100972547+christinaWyllie@users.noreply.github.com>
2022-04-04 14:14:39 -0600

    Update OrderForm.java

--- OrderForm.java
+++ OrderForm.java
@@ -36,14 +36,30 @@ public class OrderForm implements FormatOutput{
   
   @Override
   public void formatString(){
+    StringBuilder builder = new StringBuilder();
     write.append("Hamper " + n+ ":");
-    //StringBuilder builder = new StringBuilder();
     for(int i = 0; i<fam.length; i++){
          if(fam[i] != null){
            int count = fam[i];
-           write.append(n + "");
+           builder.append(n + toString(i)+ ", ");
          }
-      }
+    }
+    write.append(builder.substring(0, builder.length()-1));
   }
+  
+  public String toString(int index){
+    if(index == 0){
+      return "Adult Male";
+    }
+    else if(index ==1){
+      return "Adult Female";
+    }
+    else if(index ==2){
+      return "Child under 8";
+    }
+    else{
+      return "Child over 8";
+    }
+  } 
   
 }
