d6d466702244905018190e44dfb52c87227a8774
christinaWyllie <100972547+christinaWyllie@users.noreply.github.com>
2022-04-04 12:48:57 -0600

    Update OrderForm.java

--- OrderForm.java
+++ OrderForm.java
@@ -5,11 +5,13 @@ public class OrderForm implements FormatOutput{
   private Family family;
   private Inventory inventory;
   private HamperNutrition hamper;
+  private int numHampers;
   
-  public OrderFrom(Family f, Inventory i, HamperNutrition h)throws IOException{
+  public OrderFrom(Family f, Inventory i, HamperNutrition h, int n){
     this.family = f;
     this.inventory = i;
     this.hamper = h;
+    this.numHampers = n;
   }
   
   public void removeFromDataBase(){
@@ -19,16 +21,29 @@ public class OrderForm implements FormatOutput{
   public void print()throws IOException{
     String fileName = "orderForm.txt";
     FileWriter write = null;
+    int[] fam = family.getFamilyMembers();
     try{
       write = new FileWriter(new File(fileName));
-      
+      write.append("Name:" + "\n" + "Date:" + "\n" + "Original Request" + "\n");
+      while(n){
+        formatString();
+        n--;
+      } 
+     
     }
     
   } 
   
   @Override
   public void formatString(){
-    
+    write.append("Hamper " + n+ ":");
+    //StringBuilder builder = new StringBuilder();
+    for(int i = 0; i<fam.length; i++){
+         if(fam[i] != null){
+           int count = fam[i];
+           write.append(n + "");
+         }
+      }
   }
   
 }
