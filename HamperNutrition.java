925b9e83784e67c1a380ebc243b47bb5781a1a66
Sobia-Khan1 <101202543+Sobia-Khan1@users.noreply.github.com>
2022-04-08 08:45:04 -0600

    Update HamperNutrition.java

--- HamperNutrition.java
+++ HamperNutrition.java
@@ -1,38 +1,61 @@
 package edu.ucalgary.ensf409;
 
-public class HamperNutrition{
-  private LinkedList[] hamper = new LinkedList<Food>();
-  private Inventory inventory;
+public class HamperNutrition implements Calculate{
+  private LinkedList<Food> hamper = new LinkedList<Food>();
+  private Inventory inventory = new Inventory();
+  private Nutrition nutrition;
   
-  public HamperNutrition(Nutrition nutrition){
+  public HamperNutrition(Nutrition nutrition)
+  {
+    this.nutrition = nutrition;
+  } 
+  
+  public void calculateContent()
+  {
+ 
+  }
+  
+  public void addToHamper(Food food)
+  {
+    hamper.add(food);
+  }
+  
+  public LinkedList<Food> getHamper()
+  {
+    return this.hamper;
+  }
+  
+  public void checkInventory() throws NotEnoughInventoryException
+  {
+    if(nutrition.getGrain() > )
+      throw new NotEnoughInventoryException();
+    if(nutrition.getProtien() > )
+      throw new NotEnoughInventoryException();
     
   } 
   
-  public void calculate(){
+  public void createBestHamper()
+  {
+    //Outside ArrayList is the number of possible hampers 
+    //Inside ArrayList is the hamper with the food 
+    ArrayList<ArrayList<Food>> = new ArrayList<ArrayList<Food>>();
+    
+    for()
     
   }
   
-  public void addToHamper(Food food){
-    
+  public void removeFromInventory()
+  {
+     inventory.removeInventory();
   }
-  
-  public void checkInventory() throws NotEnoughInventoryException{
-    
-  } 
-  
-  public void createBestHamper(){
-  
-  }
-  
-  public void removeFromInventory(){
-    
-  }
-  
-  public Inventory getInventory(){
+
+  public Inventory getInventory()
+  {
     return this.inventory;
   }
   
-  public void setInventory(Inventory i){
+  public void setInventory(Inventory i)
+  {
     this.inventory = i;
   }
   
