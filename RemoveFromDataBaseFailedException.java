1af00baccfcc06103bcaba3548b5209e59c711c3
christinaWyllie <100972547+christinaWyllie@users.noreply.github.com>
2022-04-10 11:34:47 -0600

    Create RemoveFromDataBaseFailedException.java

---
+++ RemoveFromDataBaseFailedException.java
@@ -0,0 +1,26 @@
+/* 
+Group Number: 01
+Student Name: Sanika Shendye, Sobia Khan, Christina Wyllie, Maitry Rohit
+Student ID: 30114344, 30115574, 30113679, 30117194
+Email: sanika.shendye@ucalgary.ca, sobia.khan1@ucalgary.ca, christina.wyllie@ucalgary.ca, maitry.rohit@ucalgary.ca
+Course: ENSF409
+Final Project: RemoveFromDataBaseFailedException.java
+Date Submitted: April 18th, 2022
+@author Sanika Shendye <a href="mailto:sanika.shendye@ucalgary.ca">
+	sanika.shendye@ucalgary.ca</a>
+@author Sobia Khan <a href="sobia.khan1@ucalgary.ca">
+	sobia.khan1@ucalgary.ca</a>
+@author Christina Wyllie <a href="christina.wyllie@ucalgary.ca">
+	christina.wyllie@ucalgary.ca</a>
+@author Maitry Rohit <a href="mailto:maitry.rohit@ucalgary.ca">
+	maitry.rohitAucalgary.ca</a>
+@version 1.2
+@since 1.0
+ */
+
+
+public class RemoveFromDataBaseFailedException extends Exception{
+	public RemoveFromDataBaseFailedException(){
+		super("Unsuccessful removal from the DataBase");
+	}
+}
