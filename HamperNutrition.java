5f37b94b891a07af9c7d3bdb7068bd8a8802099a
Sobia-Khan1 <101202543+Sobia-Khan1@users.noreply.github.com>
2022-04-10 11:50:46 -0600

    Update HamperNutrition.java

--- HamperNutrition.java
+++ HamperNutrition.java
@@ -2,133 +2,113 @@ package edu.ucalgary.ensf409;
 
 public class HamperNutrition implements Calculate{
   private LinkedList<Food> hamper = new LinkedList<Food>();
-  private Inventory inventory = new Inventory();
-  private Nutrition nutrition;
+  private Inventory inventory;
+  private Nutrition nutrition;	//This is total nutrition from family
   
-  public HamperNutrition(Nutrition nutrition)
+  public HamperNutrition(Nutrition nutrition, Inventory inventory)	//Constructor
   {
-    this.nutrition = nutrition;
+	this.nutrition = nutrition;
+	this.inventory = inventory;
   } 
   
-  public int[] calculateContent()
+  public int[] calculateContent()	//Of Hamper
   {
-    int grain = 0;
-    int protien = 0;
-    int fAndV = 0;
-    int other = 0;
-    int calories = 0;
+    	int grain = 0;
+   	int protien = 0;
+    	int fAndV = 0;
+    	int other = 0;
+    	int calories = 0;
     
-    Iterator<Food> it = hamper.iterator();
+    	Iterator<Food> it = hamper.iterator();
     
-    while (it.hasNext())
-    {
-      Nutrition food = it.next().getNutritionalValue();
-      grain += food.getGrain();
-      protien += food.getProtien();
-      fAndV += food.getFV();
-      other += food.getOther();
-      calories += food.getCalories();
-    }
+    	while (it.hasNext())
+    	{
+      		Nutrition food = it.next().getNutritionalValue();
+      		grain += food.getGrain();
+      		protien += food.getProtien();
+      		fAndV += food.getFV();
+      		other += food.getOther();
+      		calories += food.getCalories();
+    	}
     
-    int[] contents = new int[5];
-    contents[0] = nutrition.getGrain() - grain;
-    contents[1] = nutrition.getProtien() - protien;
-    contents[2] = nutrition.getFV() - fAndV;
-    contents[3] = nutrition.getOther() - other;
-    contents[4] = nutrition.getCalories() - calories;
+    	int[] contents = new int[5];
+   	contents[0] = nutrition.getGrain() - grain;
+    	contents[1] = nutrition.getProtien() - protien;
+    	contents[2] = nutrition.getFV() - fAndV;
+    	contents[3] = nutrition.getOther() - other;
+    	contents[4] = nutrition.getCalories() - calories;
     
-    return contents;
-    
+    	return contents;
   }
   
   public void addToHamper(Food food)
   {
-    hamper.add(food);
+    	hamper.add(food);
   }
   
   public LinkedList<Food> getHamper()
   {
-    return this.hamper;
+    	return this.hamper;
   }
   
-  public void checkInventory() throws NotEnoughInventoryException
+  public void checkInventory() throws NotEnoughInventoryException	//May not need this
   {
     
   } 
   
   public void createBestHamper()
   {
-		LinkedList<Food> food = inventory.getInventory();
+	LinkedList<Food> food = inventory.getInventory()
+	int[] content = calculateContent();
+	Food best = new Food(0, 0, 0, 0, 0, 0, "nothing");
+	int b = 0;
+	
+	while(content[0] > 0 && content[1] > 0 && content[2] > 0 && content[3] > 0)	//0 - Grains, 1 - Protien, 2 - Fruits and Veg, 3 - Other
+	{										//4 - Calories - not needed becuase if other categories are satisfied calories should be satisfied	
 		Iterator<Food> it = food.iterator();
-		Food first = new Food(0, 0, 0, 0, 0, 0, "nothing");
-		
-		Food bestGrain = first;
-		Food bestProtien = first;
-		Food bestFruitAndVeg = first;
-		Food bestOther = first;
-		
 		while(it.hasNext())
 		{
-			Iterator<Food> iter = food.iterator();
-			int[] con = calculateContents();
-				
-			while(iter.hasNext())
+			Food f = it.next();
+			if(f.getNutritionalValue().getGrain() > b && f.getNutritionalValue().getGrain() <= content[0])
 			{
-					Food f = iter.next();
-					if(f.getNutritionalValue().getGrain() > bestGrain.getNutritionalValue().getGrain())
-						bestGrain =  f;
-					if(f.getNutritionalValue().getProtien() > bestProtien.getNutritionalValue().getProtien())
-						bestProtien =  f;
-					if(f.getNutritionalValue().getFV() > bestFruitAndVeg.getNutritionalValue().getFV())
-						bestFruitAndVeg =  f;
-					if(f.getNutritionalValue().getOther() > bestOther.getNutritionalValue().getOther())
-						bestOther =  f;
+				b = f.getNutritionalValue().getGrain();
+				best =  f;
 			}
-			
-			if(bestGrain.getNutritionalValue().getGrain() <= con[0])
+			if(f.getNutritionalValue().getProtien() > b && f.getNutritionalValue().getProtien() <= content[1])
 			{
-				hamper.add(bestGrain);
-				food.remove(bestGrain);
-				con = calculateContents();	//Won't work change
+				b = f.getNutritionalValue().getProtien();
+				best =  f;
 			}
-			
-			
-			if(bestGrain != bestProtien && bestProtien.getNutritionalValue().getProtien() <= con[1])
-				hamper.add(bestProtien);
-			con = calculateContents();
-			
-			if
-			
+			if(f.getNutritionalValue().getFV() > b && f.getNutritionalValue().getFV() <= content[2])
+			{
+				b = f.getNutritionalValue().getFV();
+				best =  f;
+			}
+			if(f.getNutritionalValue().getOther() > b && f.getNutritionalValue().getOther() <= content[3])
+			{
+				b = f.getNutritionalValue().getOther();
+				best =  f;
+			}
 		}
 		
-		/*
-		int i = 0;
-		
-    helpCreateBestHamper(i, food);
-    
-    ArrayList<ArrayList<Food>> = new ArrayList<ArrayList<Food>>();*/
-    
-  }
-  
-  private void helpCreateBestHamper(int i, LinkedList<Food> food)
-  {
-    int[] content = calculateContent(); 
-		
-    if(content[0] >= 0 && content[1] >= 0 && content[2] >= 0 && content[3] >= 0 && content[4] >= 0) //Add buffer
-      return;
-    else 
-    {
-      if(content[0] >= 0 || content[1] >= 0 || content[2] >= 0 || content[3] >= 0)
-			{
-				Iterator<Food> it = food.iterator();
-				
-			}
+		try
+		{
+			food.remove(best);
+			hamper.add(best);
+			content = calculateContent();
+			b = 0;
+			best = new Food(0, 0, 0, 0, 0, 0, "nothing");
 			
-			hamper.add(food.get(i));
-			i++;
-      helpCreateBestHamper(i, food);
-    }
-    
+		}
+		catch(Exception e)
+		{
+			break;
+		}
+	}
+	  
+	
+	  
+	
   }
   
   public void removeFromInventory()
