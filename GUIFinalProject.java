8b2fe6f284d22da50f24bac1e434ec590082cb54
MaitryRohit <101202431+MaitryRohit@users.noreply.github.com>
2022-04-12 12:28:30 -0600

    Update
    Fixed what wouldve been a user interface error.

--- GUIFinalProject.java
+++ GUIFinalProject.java
@@ -1,3 +1,5 @@
+package edu.ucalgary.ensf409;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -6,6 +8,7 @@ import java.awt.EventQueue;
 import javax.swing.*;
 import java.awt.event.*;
 import java.awt.FlowLayout;
+import java.util.*;
 
 public class GUIFinalProject extends JFrame implements ActionListener, MouseListener {
     private int adultM = 0;
@@ -24,22 +27,26 @@ public class GUIFinalProject extends JFrame implements ActionListener, MouseList
     private JTextField cU8Input;
     private JTextField cO8Input;
 	
-	private static boolean applicationExists = false;
+	private JButton addHamper;
+	private JButton terminate;
+	private Application application1;
+	private boolean applicationExists = false;
+	private ArrayList<int[]> family = new ArrayList<int[]>();
 
 	public GUIFinalProject(){
         super("Create an Order Form Application"); //Calling the constructor of Jframe to set up a title for the window 
         setupGUI();
-        setSize(500,300);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);        
+        setSize(1100,200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
     }
 	
 	public void setupGUI(){
         
-        instructions = new JLabel("Please enter your family information to generate a hamper.\n If you wish to add another family to your application click the button below or exit by pressing X in the top right corner.");
+        instructions = new JLabel("Please enter your family information to generate a hamper. If you wish to add another family to your application click the button below or exit by pressing X in the top right corner.");
         aMLabel = new JLabel("How many Adult Males? ");
         aFLabel = new JLabel("How many Adult Females? ");
-        cU8Label = new JLabel("How many Childern Under 8? ");
-        cO8Label = new JLabel("How many Childern Over 8?");
+        cO8Label = new JLabel("How many Children Over 8?");
+		cU8Label = new JLabel("How many Children Under 8? ");
         
         aMInput = new JTextField("e.g. 1", 10);
         aFInput = new JTextField("e.g. 0", 10);
@@ -51,12 +58,13 @@ public class GUIFinalProject extends JFrame implements ActionListener, MouseList
         cU8Input.addMouseListener(this);
         cO8Input.addMouseListener(this);
         
-    //JButton terminateApplication = new JButton("End Application");
-		//terminateApplication.addActionListener(this);
-		JButton addHamper = new JButton("Add Another Family Unit");
+     
+		addHamper = new JButton("Add Another Family Unit");
 		addHamper.addActionListener(this);
+		terminate = new JButton("Submit Application");
+		terminate.addActionListener(this);
         
-        JPanel headerPanel = new JPanel();
+		JPanel headerPanel = new JPanel();
         headerPanel.setLayout(new GridLayout());
         
         JPanel clientPanel = new JPanel();
@@ -64,7 +72,7 @@ public class GUIFinalProject extends JFrame implements ActionListener, MouseList
 
         JPanel submitPanel = new JPanel();
         submitPanel.setLayout(new FlowLayout());
-        
+		
         headerPanel.add(instructions);
         clientPanel.add(aMLabel);
         clientPanel.add(aMInput);
@@ -75,6 +83,13 @@ public class GUIFinalProject extends JFrame implements ActionListener, MouseList
         clientPanel.add(cO8Label);
         clientPanel.add(cO8Input);
 		submitPanel.add(addHamper);
+		submitPanel.add(terminate);
+		
+		headerPanel.setBackground(new Color(234, 255, 168));
+		clientPanel.setBackground(new Color(234, 255, 168));
+		submitPanel.setBackground(new Color(234, 255, 168));
+		addHamper.setBackground(new Color(164, 215, 211));
+		terminate.setBackground(new Color(164, 215, 211));
         
         this.add(headerPanel, BorderLayout.NORTH);
         this.add(clientPanel, BorderLayout.CENTER);
@@ -82,24 +97,41 @@ public class GUIFinalProject extends JFrame implements ActionListener, MouseList
     }
 	
 	 public void actionPerformed(ActionEvent event){
-        adultM = Integer.parseInt(aMInput.getText());
-        adultF = Integer.parseInt(aFInput.getText());
-        childO8 = Integer.parseInt(cO8Input.getText());
-        childU8 = Integer.parseInt(cU8Input.getText());
-        
-        if(validateInput()){
-           int[] familyArray = {adultM, adultF, childO8, childU8};
-		   /* if(!applicationExists) {
-				Application application1 = new Application();
+		boolean valid = validateInput();
+		if(valid){
+			adultM = Integer.parseInt(aMInput.getText());
+			adultF = Integer.parseInt(aFInput.getText());
+			childO8 = Integer.parseInt(cO8Input.getText());
+			childU8 = Integer.parseInt(cU8Input.getText());
+			
+			int check0 = adultM + adultF + childO8 + childU8;
+			if( check0 == 0) {
+				JOptionPane.showMessageDialog(this, "Your hamper must contain at least 1 family member.");
 			}
 			
 			else{
-				application1.addHamper();
+				int[] familyArray = {adultM, adultF, childO8, childU8};
+				family.add(familyArray);
+					
+				String message = "Your family unit is: " + adultM + " Adult Males, " + adultF + " Adult Females, " + childO8 + " Children Over 8, " + childU8 + " Children Under 8.\nSubmission for this family unit was successful.";
+				JOptionPane.showMessageDialog(this, message);
+				aMInput.setText("");
+				aFInput.setText("");
+				cU8Input.setText("");
+				cO8Input.setText("");
 			}
-			 */
-			String message = "Your family unit is: " + adultM + " Adult Males, " + adultF + " Adult Females, " + childO8 + " Childern Over 8, " + childU8 + " Childern Under 8.";
-            JOptionPane.showMessageDialog(this, message);
-        }
+		}
+		
+		if(event.getSource() == terminate) {
+			if(!valid) {
+				JOptionPane.showMessageDialog(this, "Submission for most recent unit was unsuccessful. Must submit valid entry to submit.");
+			}
+
+			else {
+				this.application1 = new Application(family);
+				super.dispose();
+			}
+		}
     }
 	
 	public void mouseClicked(MouseEvent event){
@@ -140,33 +172,45 @@ public class GUIFinalProject extends JFrame implements ActionListener, MouseList
         
         boolean allInputValid = true;
 		
-        if(adultM < 0){
+        if(!isNumeric(aMInput.getText())){
             allInputValid = false;
-            JOptionPane.showMessageDialog(this, adultM + " is not a positive integer. Input must be 0 or greater.");
+            JOptionPane.showMessageDialog(this, aMInput.getText() + " is not a valid input. Must be a positive integer.");
         }
 		
-		if(adultF < 0){
+		if(!isNumeric(aFInput.getText())){
             allInputValid = false;
-            JOptionPane.showMessageDialog(this, adultM + " is not a positive integer. Input must be 0 or greater.");
+            JOptionPane.showMessageDialog(this, aFInput.getText() + " is not a valid input. Must be a positive integer.");
         }
 		
-		if(childU8 < 0){
+		if(!isNumeric(cO8Input.getText())){
             allInputValid = false;
-            JOptionPane.showMessageDialog(this, adultM + " is not a positive integer. Input must be 0 or greater.");
+            JOptionPane.showMessageDialog(this, cO8Input.getText() + " is not a valid input. Must be a positive integer.");
         }
 		
-		 if(childO8 < 0){
+		 if(!isNumeric(cU8Input.getText())){
             allInputValid = false;
-            JOptionPane.showMessageDialog(this, adultM + " is not a positive integer. Input must be 0 or greater.");
+            JOptionPane.showMessageDialog(this, cU8Input.getText() + " is not a valid input. Must be a positive integer.");
         }
 		
         return allInputValid;
         
     }
 	
+	public boolean isNumeric(String input) {
+		boolean result = true;
+		try {
+			int check = Integer.parseInt(input);
+			if(check < 0) {
+				result = false;
+			}
+		}
+		catch (Exception e) {
+			result = false;
+		}
+		return result;
+	}
 	
     public static void main(String args[]) {
-
         EventQueue.invokeLater(() -> {
             new GUIFinalProject().setVisible(true); //Create everything and put it in then set the visibility to true 
         });
