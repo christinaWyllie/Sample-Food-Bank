a1f6dd8c28dce6be3156d8759230d80912f2146f
MaitryRohit <101202431+MaitryRohit@users.noreply.github.com>
2022-04-13 20:52:34 -0600

    Update Application.java

--- Application.java
+++ Application.java
@@ -21,6 +21,7 @@ Date Submitted: April 18th, 2022
 package edu.ucalgary.ensf409;
 
 import java.util.*;
+import java.io.*;
 
 public class Application{
   
@@ -28,24 +29,38 @@ public class Application{
   private Inventory inventory;
   private ArrayList<Family> families;
   private ArrayList<HamperNutrition> hamper;
+  //private int[][] calorieInfo = {{1,30,20,40,10,2500}, {2,20,40,10,30,2500}, {3,21,33,31,15,2200}, {4,21,33,21,15,1400}};
   
   public Application() {}
-  public Application(ArrayList<int[]> var){
+  public Application(ArrayList<int[]> var) throws NotEnoughInventoryException, RemoveFromDataBaseFailedException{
     this.inventory = new Inventory();
     int[][] calorieTable = inventory.getCalorieTable();
+	
     this.families = new ArrayList<Family>();
 	for (int[] array : var) {
 		this.families.add(new Family(array, calorieTable));
 	}
-    this.hamper = new ArrayList<HamperNutrition>();
-    this.hamper.add(new HamperNutrition(this.families.get(numHampers-1).getTotal()));
+	
+	this.numHamper = families.size();
+	
+	this.hamper = new ArrayList<HamperNutrition>();
+	int i = 0;
+	for (Family family : families) {
+		this.hamper.add(new HamperNutrition(family.getTotal(), inventory));
+		hamper.get(i).createBestHamper();
+		i++;
+		
+	}
+	
+	OrderForm order = new OrderForm(families, inventory, hamper, numHamper);
+	order.removeFromDataBase();
   }
   
-  public int[] getUserInput(int index) {return this.families.get(index).getFamilyMembers();}
+  public int[] getUserInput(int index) {return this.families.get(index).getID();}
   public int getNumHamper() {return this.numHamper;}
-  public ArrayList<int[]> getFamilies() {return this.families;}
-  public Arraylist<HamperNutrition> getHampers() {return this.hamper;}
-  public int[] getFamily(int index) {return this.families.get(index);}
+  public ArrayList<Family> getFamilies() {return this.families;}
+  public ArrayList<HamperNutrition> getHampers() {return this.hamper;}
+  public Family getFamily(int index) {return this.families.get(index);}
   public LinkedList<Food> getHamper(int index) {return this.hamper.get(index).getHamper();}
   public Inventory getInventory() {return this.inventory;}
   
