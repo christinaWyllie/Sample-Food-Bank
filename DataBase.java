2e7a45ee575cfe55b983972b8f53ef02fbb5e745
sanikashendye <101202484+sanikashendye@users.noreply.github.com>
2022-04-13 20:13:59 -0600

    Update DataBase.java

--- DataBase.java
+++ DataBase.java
@@ -1,4 +1,4 @@
-/* 
+/**
 Group Number: 01
 Student Name: Sanika Shendye, Sobia Khan, Christina Wyllie, Maitry Rohit
 Student ID: 30114344, 30115574, 30113679, 30117194
@@ -27,26 +27,27 @@ public class DataBase{
   private Connection dbConnect;
   private ResultSet results;
   private int[][] calorieTable = new int[4][6]; 
-  private LinkedList<Food> inventory = new LinkedList<Food>(); //where we will pass this info 
+  private LinkedList<Food> inventory = new LinkedList<Food>(); //where we will pass the database info 
   
   
-  public DataBase(){
-    initializeConnection();
-    getCalorieData();
-    getInventoryData();
+  public DataBase(){ //class constructor
+    initializeConnection(); //initializes sql connection to database with user student, password ensf
+    getCalorieData(); //gets calorie needs information from database 
+    getInventoryData(); //gets inventory information from the database
   }
   
+//initializes sql connection to database with user student, password ensf
   private void initializeConnection(){
     try{
       this.dbConnect = DriverManager.getConnection("jdbc:mysql://localhost/food_inventory", "student", "ensf"); 
     }
     catch(SQLException e){
       System.out.println("Error connecting to database.");
-      e.printStackTrace(); //may need to be changed 
+      e.printStackTrace(); 
     }
     
   }
-  
+  //gets calorie needs information from database 
   private void getCalorieData(){
     try{
       Statement myStmt = this.dbConnect.createStatement();
@@ -54,7 +55,7 @@ public class DataBase{
       this.results = myStmt.executeQuery(query);
       int i = 0; 
       
-      while(this.results.next()){
+      while(this.results.next()){ //puts results into an integer nested array
         int g = results.getInt("WholeGrains");
         int p = results.getInt("Protein");
         int f = results.getInt("FruitVeggies");
@@ -75,7 +76,8 @@ public class DataBase{
         }
 
     }
-    
+	
+    //gets inventory information from the database
     public void getInventoryData(){
       try{
       Statement myStmt = this.dbConnect.createStatement();
@@ -89,29 +91,38 @@ public class DataBase{
         int f = results.getInt("FVContent");
         int o = results.getInt("Other");
         int c = results.getInt("Calories");
-        Food foodItem = new Food(id, name, g, f, p, o, c);
-        this.inventory.add(foodItem);
+        Food foodItem = new Food(id, name, g, f, p, o, c); //creates food item with each item in the database
+        this.inventory.add(foodItem); //stores food item in linkedlist of food objects
         }
       }catch (SQLException ex) {
           //ex.printStackTrace();
        }
     }
-    
+	
+    //returns calorie needs for all possible family members
     public int[][] getCalorieInfo()
     {
-      return this.calorieTable;
+      return this.calorieTable; 
     }
+	
+    //returns list of items in the inventory in database
     public LinkedList<Food> getInventoryInfo(){
-      return this.inventory; //make sure that copy is deep???
+      return this.inventory;
     }
     
+     //is called after all hampers are created using the linkedlist, this is the final stage where the 
+     //database is fully updated and the used hamper items are removed
+	
+	/**
+	* @param LinkedList<Food> toBeRemoved
+	*/
     public boolean updateDataBase(LinkedList<Food> toBeRemoved){
       boolean success = false;
-      for(int k = 0; k < toBeRemoved.size(); k++)
+      for(int k = 0; k < toBeRemoved.size(); k++) //iterated through the list of items to be removed from db
       {
          
         try{
-         String query = "DELETE FROM AVAILABLE_FOOD WHERE ItemID = ? AND Name = ?";
+         String query = "DELETE FROM AVAILABLE_FOOD WHERE ItemID = ? AND Name = ?"; //matches name and id 
             PreparedStatement myStmt = dbConnect.prepareStatement(query);
 
             myStmt.setString(1, Integer.toString(toBeRemoved.get(k).getFoodID()));
@@ -124,8 +135,8 @@ public class DataBase{
               return false;
             }
             else{
-				success = true;
-			}
+		success = true;
+		}
    
             
             myStmt.close();
@@ -137,19 +148,21 @@ public class DataBase{
       }
       if (success == true)
       {
-        removeInventory();
+        removeInventory(); //clears the inventory structure stored within database
       }
 		  
       return success;
     }
-  
-    private void removeInventory() //clears inventory we have stored in structure and re reads from database after deletions
+	
+	
+    //clears inventory we have stored in structure and re reads from database after deletions
+    private void removeInventory() 
     {
       this.inventory.clear();
       getInventoryData(); //inv stored in this structure now should match whats in inventory class structure
     }
-  
-    public void close() { //need to add to UML and add as a call when terminating application/orderform 
+  //closes the connection to the database at the completion of an order
+    public void close() { 
 		  try {
             this.results.close();
             this.dbConnect.close();
