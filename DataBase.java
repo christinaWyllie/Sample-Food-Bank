e6c1ad0d4da9c6a337feec6ab9c87d092f25e68d
sanikashendye <101202484+sanikashendye@users.noreply.github.com>
2022-04-14 20:56:10 -0600

    Update DataBase.java

--- DataBase.java
+++ DataBase.java
@@ -23,21 +23,35 @@ package edu.ucalgary.ensf409;
 import java.sql.*;
 import java.util.*;
 
+/**
+ * This class is responsible for maintaining the connection to the 
+ * database and updating the database if an order is able to be fullfilled.
+ * This class returns the inventory information and caloric information from 
+ * the database.
+ */
 public class DataBase{
   private Connection dbConnect;
   private ResultSet results;
   private int[][] calorieTable = new int[4][6]; 
   private LinkedList<Food> inventory = new LinkedList<Food>(); //where we will pass the database info 
   
-  
-  public DataBase(){ //class constructor
+  /**
+   * Sole class constructor that initializes the connection to the 
+   * database and reads the client caloric needs and the initial
+   * inventory that is present in the database.
+   */
+  public DataBase(){ 
     initializeConnection(); //initializes sql connection to database with user student, password ensf
     getCalorieData(); //gets calorie needs information from database 
     getInventoryData(); //gets inventory information from the database
   }
   
-//initializes sql connection to database with user student, password ensf
-  private void initializeConnection(){
+
+	/**
+	* This method initializes the driver connection to the database using 
+	* the username 'student' and password 'ensf'.
+	*/
+	private void initializeConnection(){
     try{
       this.dbConnect = DriverManager.getConnection("jdbc:mysql://localhost/food_inventory", "student", "ensf"); 
     }
@@ -47,37 +61,47 @@ public class DataBase{
     }
     
   }
-  //gets calorie needs information from database 
-  private void getCalorieData(){
-    try{
-      Statement myStmt = this.dbConnect.createStatement();
-      String query = "SELECT * FROM DAILY_CLIENT_NEEDS";
-      this.results = myStmt.executeQuery(query);
-      int i = 0; 
-      
-      while(this.results.next()){ //puts results into an integer nested array
-        int g = results.getInt("WholeGrains");
-        int p = results.getInt("Protein");
-        int f = results.getInt("FruitVeggies");
-        int o = results.getInt("Other");
-        int c = results.getInt("Calories");
-        
-        this.calorieTable[i][0] = i + 1;
-        this.calorieTable[i][1] = g;
-        this.calorieTable[i][2] = f;
-        this.calorieTable[i][3] = p;
-        this.calorieTable[i][4] = o;
-        this.calorieTable[i][5] = c;
-        
-        i++;
-	}
-     }catch (SQLException ex) {
-            ex.printStackTrace();
-        }
-
+  
+   /**
+     * This method queries through the daily client caloric needs
+     * and stores the information in a nested integer array that is
+     * organized by caloric needs per category and client ID number.
+     */
+	private void getCalorieData(){
+		try{
+		  Statement myStmt = this.dbConnect.createStatement();
+		  String query = "SELECT * FROM DAILY_CLIENT_NEEDS";
+		  this.results = myStmt.executeQuery(query);
+		  int i = 0; 
+		  
+		  while(this.results.next()){ //puts results into an integer nested array
+			int g = results.getInt("WholeGrains");
+			int p = results.getInt("Protein");
+			int f = results.getInt("FruitVeggies");
+			int o = results.getInt("Other");
+			int c = results.getInt("Calories");
+			
+			this.calorieTable[i][0] = i + 1;
+			this.calorieTable[i][1] = g;
+			this.calorieTable[i][2] = f;
+			this.calorieTable[i][3] = p;
+			this.calorieTable[i][4] = o;
+			this.calorieTable[i][5] = c;
+			
+				i++;
+			
+			 }catch (SQLException ex) {
+					ex.printStackTrace();
+				}
+		}
     }
 	
-    //gets inventory information from the database
+    /**
+     * This method queries through the available food items that are present
+     * in the database and stores them in a linked list of food objects
+     * by iterating through each entry and calling the Food class 
+     * constructor per entry.
+     */
     public void getInventoryData(){
       try{
       Statement myStmt = this.dbConnect.createStatement();
@@ -99,22 +123,41 @@ public class DataBase{
        }
     }
 	
-    //returns calorie needs for all possible family members
+    
+    /**
+     * This method returns the calorie table of the daily needs of all
+     * possible family members that has been read and stored from the 
+     * database.
+     * 
+     * @return 	returns an int array that contains daily caloric needs of family members
+     */
     public int[][] getCalorieInfo()
     {
       return this.calorieTable; 
     }
 	
-    //returns list of items in the inventory in database
+    
+    /** 
+     * This method returns a LinkedList of food items that is indicative
+     * of the inventory that is available in the database.
+     * 
+     * @return 	returns a linked list of food objects
+     */
     public LinkedList<Food> getInventoryInfo(){
       return this.inventory;
     }
     
-     //is called after all hampers are created using the linkedlist, this is the final stage where the 
-     //database is fully updated and the used hamper items are removed
-	
+    
 	/**
-	* @param LinkedList<Food> toBeRemoved
+	 * This method iterates through the list of inventory to be removed that is
+	 * passed to it and queries through the database, matching the name of the 
+	 * food and the ID number in order to remove the item from the database. Upon
+	 * successful removal of the entire list, a boolean true value is returned.
+	 * This method is called after all hampers are created and determined that 
+	 * they are able to be fullfilled.
+	 * 
+	* @param toBeRemoved  	a linked list of food items that will be iterated through and removed from the database
+	* @return 	returns true if removal was successful, returns false otherwise
 	*/
     public boolean updateDataBase(LinkedList<Food> toBeRemoved){
       boolean success = false;
@@ -155,13 +198,22 @@ public class DataBase{
     }
 	
 	
-    //clears inventory we have stored in structure and re reads from database after deletions
+
+    /**
+     * This method clears the linked list stored in the structure and rereads from 
+     * the database after the deletion to ensure that the most updated version of the
+     * database is used.
+     */
     private void removeInventory() 
     {
       this.inventory.clear();
       getInventoryData(); //inv stored in this structure now should match whats in inventory class structure
     }
-  //closes the connection to the database at the completion of an order
+    
+ 
+  /**
+   * This method closes the connection to the database upon completion of the order.
+   */
     public void close() { 
 		  try {
             this.results.close();
@@ -171,5 +223,3 @@ public class DataBase{
         }
      }
   }
-
-     
