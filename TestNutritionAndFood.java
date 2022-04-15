915becfe4293fa10861d251a304a996548220231
sanikashendye <101202484+sanikashendye@users.noreply.github.com>
2022-04-15 17:10:06 -0600

    Update TestNutritionAndFood.java

--- TestNutritionAndFood.java
+++ TestNutritionAndFood.java
@@ -27,37 +27,51 @@ import java.io.*;
 import java.util.*;
 
 public class TestNutritionAndFood{
+	
 	//NUTRITION TESTS
 	@Test
-    public void testGetNutritionSinglePerson() {
-    //tests getters when only one person is given to the application 
-	//NO addition required for totals 
-    int givenDataGrain = 400;
-    int givenDataFV = 700;
+	//This test tests that the nutrition constructor creates and returns a valid object
+    public void testtNutritionConstructor() {
+	int givenDataGrain = 400;
+    	int givenDataFV = 700;
 	int givenDataProtein = 650;
 	int givenDataOther = 750;
-	int givenDataCalories = 2500;
-		
-	int[] array = {1,0,0,0};
+	int givenDataCalories = 2500; 
+	    
+	Nutrition nutrition = new Nutrition(givenDataGrain, givenDataFV, givenDataProtein, givenDataOther, givenDataCalories);
+	 assertNotNull("The Nutriton object created by the constructor is null.", nutrition);
+    }
+	    
+	//NUTRITION TESTS
+	@Test
+    public void testNutritionGetters() {
+    //tests getters when only one person is given to the application 
+	//NO addition required for totals, since nutrition is a container class
+    	int givenDataGrain = 400;
+    	int givenDataFV = 700;
+	int givenDataProtein = 650;
+	int givenDataOther = 750;
+	int givenDataCalories = 2500; 
+	    
 	Nutrition nutrition = new Nutrition(givenDataGrain, givenDataFV, givenDataProtein, givenDataOther, givenDataCalories);
 		
 	int actualContentGrain = nutrition.getGrain();
-    int actualContentProtein = nutrition.getProtein();
+    	int actualContentProtein = nutrition.getProtein();
 	int actualContentFV = nutrition.getFV();
 	int actualContentOther = nutrition.getOther();
 	int actualContentCalories = nutrition.getCalories();
         
-
-    assertEquals("Value of grains did not match what was expected: ", givenDataGrain, actualContentGrain);
-	assertEquals("Value of protein did not match what was expected: ", givenDataFV, actualContentFV);
-	assertEquals("Value of FV content did not match what was expected: ", givenDataProtein, actualContentProtein);
-	assertEquals("Value of other nutrition did not match what was expected: ", givenDataOther, actualContentOther);
-	assertEquals("Value of calories did not match what was expected: ", givenDataCalories, actualContentCalories);
+	//assert that all getters return the correct values that are set by the constructor in nutrition
+   	assertEquals("Value of grains does not match what was expected: ", givenDataGrain, actualContentGrain);
+	assertEquals("Value of protein does not match what was expected: ", givenDataFV, actualContentFV);
+	assertEquals("Value of FV content does not match what was expected: ", givenDataProtein, actualContentProtein);
+	assertEquals("Value of other nutrition does not match what was expected: ", givenDataOther, actualContentOther);
+	assertEquals("Value of calories does not match what was expected: ", givenDataCalories, actualContentCalories);
     }
 	
 	//FOOD TEST
 	@Test
-	public void testFoodConstructorandGetter(){
+	public void testFoodConstructor(){
 	//tests that the food constructor creates a valid object
 	   String name1= "Wheat bread, loaf";
 	   int id = 1;
@@ -67,21 +81,23 @@ public class TestNutritionAndFood{
 	   int o1 = 0;
 	   int c1 = 2192;
 	   Food food1 = new Food(id, name1, g1, f1, p1, o1, c1);
-	   assertNotNull("Food did not create a valid object.", food1);
+	   assertNotNull("Food constructor does not create a valid object.", food1);
    	}
 			   
 	//FOOD TEST 
   	 @Test
-   	public void testFoodGetters(){
-	//tests that the getters in food returns the expected nutrition object
+   	public void testFoodGettersAndCalculateContentMethod(){
+	//tests that the getters in food returns the expected nutrition object, and values that were set percentage wise by food
 	   String name1= "Granola Bar, 1 box";
 	   int expectedid = 2;
+	//these values are percentages
 	   int g1 = 12;
 	   int f1 = 12;
 	   int p1 = 56;
 	   int o1 = 20;
 	   int c1 = 1000;
 	   Food food1 = new Food(expectedid, name1, g1, f1, p1, o1, c1);
+	//these are expected values, Food should return the same values as calculated in the calculateContent method
 	   int gval = (int)((g1/100.0) * c1);
 	   int fval = (int)((f1/100.0) * c1);
 	   int pval = (int)((p1/100.0) * c1);
@@ -89,12 +105,18 @@ public class TestNutritionAndFood{
 	   Nutrition expected = new Nutrition(gval, fval, pval, oval, c1);
 	   Nutrition actual = food1.getNutritionalValue();
 	   int actualID = food1.getFoodID();
-	    assertEquals("Value of grains did not match what was expected: ", gval, actual.getGrain());
-		assertEquals("Value of protein did not match what was expected: ", pval, actual.getProtein());
-		assertEquals("Value of FV content did not match what was expected: ", fval, actual.getFV());
-		assertEquals("Value of other nutrition did not match what was expected: ", oval, actual.getOther());
-		assertEquals("Value of calories did not match what was expected: ", c1, actual.getCalories());
+	   String actualName = food1.getName();
+	
+		//asserts that the getters within food getNutrtionalValue(), and getName() and getFoodID() match expected results
+		//also checks that the values produced by the calculateContent method that are then stored in the nutrition 
+		//object are correct and match the expected
+	    assertEquals("Value of grains does not match what was expected: ", gval, actual.getGrain());
+		assertEquals("Value of protein does not match what was expected: ", pval, actual.getProtein());
+		assertEquals("Value of FV content does not match what was expected: ", fval, actual.getFV());
+		assertEquals("Value of other nutrition does not match what was expected: ", oval, actual.getOther());
+		assertEquals("Value of calories does not match what was expected: ", c1, actual.getCalories());
 		assertEquals("Food ID does not match expected.", expectedid, actualID);
+		assertEquals("Name of food does not match expected.", name1, actualName);
    }
    @Test
    public void testFoodImplementsCalculate(){
@@ -107,6 +129,6 @@ public class TestNutritionAndFood{
 	   int o1 = 0;
 	   int c1 = 2192;
 	   Food food1 = new Food(id, name1, g1, f1, p1, o1, c1);
-		assertTrue("Food does not implement Calculate", (Calculate.class.isAssignableFrom(food1.getClass())));
+	assertTrue("Food does not implement Calculate", (Calculate.class.isAssignableFrom(food1.getClass())));
 	}
 }
