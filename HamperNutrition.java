3e236da3adcadf5a3ca61529f1724a9184be279a
sanikashendye <101202484+sanikashendye@users.noreply.github.com>
2022-04-14 21:26:59 -0600

    Update HamperNutrition.java

--- HamperNutrition.java
+++ HamperNutrition.java
@@ -1,12 +1,42 @@
+/**
+Group Number: 01
+Student Name: Sanika Shendye, Sobia Khan, Christina Wyllie, Maitry Rohit
+Student ID: 30114344, 30115574, 30113679, 30117194
+Email: sanika.shendye@ucalgary.ca, sobia.khan1@ucalgary.ca, christina.wyllie@ucalgary.ca, maitry.rohit@ucalgary.ca
+Course: ENSF409
+Final Project: FormatOutput.java
+Date Submitted: April 18th, 2022
+@author Sanika Shendye <a href="mailto:sanika.shendye@ucalgary.ca">
+	sanika.shendye@ucalgary.ca</a>
+@author Sobia Khan <a href="sobia.khan1@ucalgary.ca">
+	sobia.khan1@ucalgary.ca</a>
+@author Christina Wyllie <a href="christina.wyllie@ucalgary.ca">
+	christina.wyllie@ucalgary.ca</a>
+@author Maitry Rohit <a href="mailto:maitry.rohit@ucalgary.ca">
+	maitry.rohitAucalgary.ca</a>
+@version 1.2
+@since 1.0
+ */
+
 package edu.ucalgary.ensf409;
 import java.util.*;
 
+/**
+ * This class uses the caloric information determined for the entire
+ * family and the available inventory in the database to create a hamper
+ * of food items that best meets the needs of the family with the least 
+ * overage in terms of calories per category.
+ */
 public class HamperNutrition {
   private LinkedList<Food> hamper = new LinkedList<Food>();
   private Inventory inventory;
   private Nutrition nutrition;	//This is total nutrition from family
   private LinkedList<Food> food;
   
+  /**
+   * Sole class constructor that initializes the nutrition information 
+   * and inventory for use in calculation of the hamper.
+   */
   public HamperNutrition(Nutrition nutrition, Inventory inventory)	//Constructor
   {
 	this.nutrition = nutrition;
@@ -14,6 +44,14 @@ public class HamperNutrition {
 	this.food = updateInventory();
   } 
   
+  /**
+   * This method makes sure that the most updated version of the
+   * inventory linked list from the inventory class is used in further calculation
+   * of hampers, so that only items that have not yet been placed in a hamper 
+   * are used in calculation of future hampers.
+   * 
+   * @return 	returns a linked list of food that has not yet been used in a hamper
+   */
   private LinkedList<Food> updateInventory()
   {
 		LinkedList<Food> remove = inventory.getRemoveInventory();
@@ -34,6 +72,13 @@ public class HamperNutrition {
 		return inv;
   }
 
+/**
+ * This method returns the amount of calories that have been fullfilled by
+ * the hamper order, seperated by category. Negative values mean that the minimum
+ * number of calories in that category have been fullfilled.
+ * 
+ * @return 	returns an integer array of the caloric content that has been fullfilled so far per category
+ */
   public int[] calculateContent()	//Of Hamper
   {
     	int grain = 0;
@@ -55,7 +100,7 @@ public class HamperNutrition {
     	}
     
     	int[] contents = new int[5];
-   	contents[0] = nutrition.getGrain() - grain;
+		contents[0] = nutrition.getGrain() - grain;
     	contents[1] = nutrition.getProtein() - protein;
     	contents[2] = nutrition.getFV() - fAndV;
     	contents[3] = nutrition.getOther() - other;
@@ -64,16 +109,34 @@ public class HamperNutrition {
     	return contents;
   }
   
+  /**
+   * This method adds a food item to the hamper being created.
+   * 
+   * @param food 	a food object that needs to be added to the hamper being created
+   */
   public void addToHamper(Food food)
   {
     	hamper.add(food);
   }
   
+  /**
+   * This method returns the hamper that has been created according to 
+   * the given input caloric needs.
+   * 
+   * @return 	returns a linked list of food items that is the hamper for the order
+   */
   public LinkedList<Food> getHamper()
   {
     	return this.hamper;
   }
   
+  /**
+   * This method checks that all minimum caloric amounts per category
+   * have been met, after the best hamper is created. If any category 
+   * was not able to be fullfilled, then an exception is thrown.
+   * 
+   * @throws NotEnoughInventoryException	throws if the order cannot meet all caloric needs
+   */
   public void checkInventory() throws NotEnoughInventoryException
   {
     	int[] content = calculateContent();
@@ -84,6 +147,12 @@ public class HamperNutrition {
 	return;
   } 
   
+  /**
+   * This method creates the best possible hamper for the given caloric needs
+   * that hits the minimum needs per category, while reducing overage by iterating
+   * through the available inventory to select the best possible combination to 
+   * put in the hamper.
+   */
   public void createBestHamper()
   {
 	//LinkedList<Food> food = inventory.getInventory();
@@ -218,6 +287,12 @@ public class HamperNutrition {
 	}
   }
   
+  /**
+   * This method iterates through the best hamper that has been created, and 
+   * for each item in the hamper, tries to determine the caloric content per 
+   * category if the item is removed. If the hamper can still meet all needs
+   * when the item is removed, the item is then taken out of the hamper.
+   */
  private void removeExtraFromHamper()
   {
 	LinkedList<Food> betterHamper = copyHamper();
@@ -234,7 +309,12 @@ public class HamperNutrition {
 			food.add(f);
 	}
   }
-  
+  /**
+   * This method creates a new linked list of food objects that is a deep 
+   * copy of the created hamper. 
+   * 
+   * @return  returns a linked list of food objects that is a deep copy of the hamper
+   */
   public LinkedList<Food> copyHamper()	//Creates deep copy of hamper
   {
 	Iterator<Food> it = hamper.iterator();
@@ -248,17 +328,33 @@ public class HamperNutrition {
 	return hamperCopy;
   }	
 
- 	
+ /**
+  * This method calls the remove method from the inventory object to 
+  * update the inventory list in the inventory class to reflect the available
+  * inventory that is left after the creation of a hamper.
+  */
   public void removeFromInventory()
   {
 	inventory.removeInventory();
   }
 
+/**
+ * This method returns the inventory object stored within the HamperNutrition
+ * class.
+ * 
+ * @return 	returns an inventory object that is used in creation of the hamper
+ */
   public Inventory getInventory()
   {
 	return this.inventory;
   }
   
+ /**
+ * This method sets the inventory object stored within the HamperNutrition
+ * class.
+ * 
+ * @param i 	Inventory object that is used in creation of the hamper
+ */
   public void setInventory(Inventory i)
   {
 	this.inventory = i;
