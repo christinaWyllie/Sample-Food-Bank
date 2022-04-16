acf3316227f3c22d9d4d6959b3a99bc8586a685e
christinaWyllie <100972547+christinaWyllie@users.noreply.github.com>
2022-04-16 11:40:28 -0600

    Update Application.java

--- Application.java
+++ Application.java
@@ -38,7 +38,7 @@ public class Application{
   private ArrayList<Family> families; //Array that holds family objects where each family is a user input containing nutrition for amount of members specified
   private ArrayList<HamperNutrition> hamper; //Array that holds the "best hamper" for each family object specified (ArrayList for both families 
 	//and hamper correspond to eachother i.e index 0 holds best hamper for Family in families[0]
-  private OrderForm order;
+
 	
   //private int[][] calorieInfo = {{1,30,20,40,10,2500}, {2,20,40,10,30,2500}, {3,21,33,31,15,2200}, {4,21,33,21,15,1400}};
   
